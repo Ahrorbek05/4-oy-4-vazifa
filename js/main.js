b0e27d4ee261ae7694a63602ecb9bacde64d7b68
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-05-24 00:47:19 +0500

    uy ishi

---
+++ js/main.js
@@ -0,0 +1,92 @@
+// 1-masala
+// let t = +prompt('Poyezdning ketish vaqti');
+// t >= 0 || t <= 23;
+// let T = +prompt('Kech qolgan vaqti');
+// T ( 1 <= T || T >= 1000 );
+// (t + T)%24;
+// console.log(t);
+// console.log(T);
+
+// 2-masala
+// let C = +prompt('gradusni selsiy shkalasida kiriting');
+// let = k;
+// let = f;
+// k = C + 273.15;
+// k = C * 1.80 + 32.00;
+// console.log(k);
+// console.log(f);
+
+// 3-masala
+// let a = +prompt('a kiritilsin');
+// let b = +prompt('b kiritilsin');
+// let p;
+// let s;
+// p = 2 * ( a + b );
+// s = a * b ;
+// if ( p > s ){
+//     console.log('perimetri');
+// } else {
+//     console.log('yuzasi');
+// }
+
+// 4-masala
+// const n = +prompt('n'); 
+// const a = +prompt('a'); 
+// const b = +prompt('b'); 
+//     const Weight = n * (1 - a / 100);
+//     const weight = weight / (1 - b / 100);
+//     console.log(weight);
+//     console.log(Weight);
+
+
+// 5-masala
+// let N = +prompt("N ni kiriting");
+// let B = N * (N-2); 
+// console.log(B);
+
+// 6-masala
+// function vaqtniTekshirish(t) {
+//     if (t < 0) {
+//       console.log("vaqt salbiy bolishi mumkin emas.");
+//     } else if (t === 0) {
+//       console.log("Vaqt o'tmagan.");
+//     } else if (t > 0 && t <= 30) {
+//       console.log("Qisqa vaqt o'tdi.");
+//     } else if (t > 30 && t <= 60) {
+//       console.log("Taxminan bir soat o'tdi.");
+//     } else {
+//       console.log("Bir soatdan ko'proq vaqt o'tdi.");
+//     }
+//   }
+
+// 7-masala
+// let B = +prompt("B");
+// let S =  +prompt("S");
+// let Y = (B-S);
+// let H = (Y/2)
+// console.log(H);
+
+// 8-masala
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let T = +prompt('T');
+// let result;
+
+// result = (A - T)/(B + A);
+// console.log(result);
+
+// 9-masala
+// let V = +prompt("V");
+// let A =  +prompt("A");
+// let B =  +prompt("B");
+// let G = (B-V);
+// let K = (A-V);
+// console.log(G);
+// console.log(K);
+
+// 10-masala
+// let rector = +prompt('rector ni kiriting.');
+// let tester = +prompt('tester ni kiriting.');
+// let result;
+// result = rector + tester / 2;
+// console.log(result);
